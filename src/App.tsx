a7c4469c37e36146534a7e706f4287ed0a8e21bf
George Muresan <c_George.Muresan@viasat.com>
2023-03-27 10:19:57 -0400

    bootstrap with CreateReactApp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import CandidateChooser from "./components/CandidateChooser";
+
+function App() {
+  return (
+    <div className="App">
+      <CandidateChooser />
+    </div>
+  );
+}
+
+export default App;
